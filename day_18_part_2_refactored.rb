9a6929c8bc50f61c193915ce9737fb09ab55d706
Andrew Baber <andrewb@thelevelup.com>
2017-12-19 14:01:45 -0500

    Give up on refactor, steal from austen

--- day_18_part_2_refactored.rb
+++ day_18_part_2_refactored.rb
@@ -1,4 +1,4 @@
-@input = <<-EOM
+$input = <<-EOM
 set i 31
 set a 1
 mul p 17
@@ -42,97 +42,145 @@ jgz f -16
 jgz a -19
 EOM
 
-# @input = <<-EOM
-# set a 1
-# add a 2
-# mul a a
-# mod a 5
-# snd a
-# set a 0
-# rcv a
-# jgz a -1
-# set a 1
-# jgz a -2
-# EOM
+# ok time to give up on refactoring this back into a workable solution. stealing it from austen
+# fml
 
-@instructions = []
-@input.chomp.split("\n").each_with_index do |instruction,i|
-  compiled = instruction.split(' ')
-  compiled << i
-  @instructions << compiled
-end
 
-@registers = Hash.new 0
-letters = ['a','i','p','b','f']
-letters.each do |l|
-  @registers[l] = 0
-end
+class SoundProcessingUnit
+  def initialize ab
+    @instructions = []
+    @program_counter = 0
+    @register_file = Hash.new(0)
+    @register_file['p'] = ab
 
-def integer?(input)
-  input.to_i.to_s == input
-end
+    @io_queue = []
+    @io_target = nil
+    @io_blocking = false
+    @running = true
+    @send_count = 0
+  end
 
-def do_instructions
-  start_index = @start_index
-  @instructions[start_index..-1].each do |ins|
+  def pair_io io_target
+    @io_target = io_target
+  end
 
-    case ins[0]
+  def send_io arg
+    if @io_target != nil
+      @io_target.io(arg)
+    end
+  end
+
+  def io arg
+    @io_queue.push(arg)
+  end
+
+  def get_value arg
+    v = nil
+    if /([a-z]+)/.match arg.to_s
+      v = @register_file[arg]
+    else
+      v = arg.to_i
+    end
+    return v
+  end
+
+  def exec(cmd, arg1, arg2)
+    v = nil
+    if arg2 != nil
+      v = get_value(arg2)
+    end
+
+    case cmd
     when 'snd'
-      # NEW SEND INSTRUCTIONS
+      send_io(get_value(arg1))
+      @send_count += 1
     when 'set'
-      if integer?(ins[2])
-        @registers[ins[1]] = ins[2]
-      else
-        @registers[ins[1]] = @registers[ins[2]]
-      end
+      @register_file[arg1] = v.to_i
     when 'add'
-      if integer?(ins[2])
-        @registers[ins[1]] = @registers[ins[1]].to_i + ins[2].to_i
-      else
-        @registers[ins[1]] = @registers[ins[1]].to_i + @registers[ins[2]].to_i
-      end
+      @register_file[arg1] += v.to_i
     when 'mul'
-      if integer?(ins[2])
-        @registers[ins[1]] = @registers[ins[1]].to_i * ins[2].to_i
-      else
-        @registers[ins[1]] = @registers[ins[1]].to_i * @registers[ins[2]].to_i
-      end
+      @register_file[arg1] = @register_file[arg1].to_i * v.to_i
     when 'mod'
-      if integer?(ins[2])
-        @registers[ins[1]] = (@registers[ins[1]].to_i % ins[2].to_i)
-      else
-        result = (@registers[ins[1]].to_i % @registers[ins[2]].to_i)
-        puts result
-        @registers[ins[1]] = result
-      end
+      @register_file[arg1] = @register_file[arg1].to_i % v.to_i
     when 'rcv'
-      #NEW RECEIVE INSTRUCTIONS
+      if @io_queue.empty?
+        @io_blocking = true
+        return
+      else
+        @io_blocking = false
+        @register_file[arg1] = get_value(@io_queue.shift()).to_i
+      end
     when 'jgz'
-      if (integer?(ins[1]) && ins[1].to_i > 0) || @registers[ins[1]].to_i > 0
-        if ins[2].to_i.to_s == ins[2]
-          @start_index = (ins[2].to_i + ins[3] -1)
-        else
-          @start_index = (@registers[ins[2]] + ins[3])
-        end
+      if get_value(arg1) > 0 then
+        @program_counter += get_value(arg2)
+        return
+      end
+    end
+    @program_counter += 1
+  end
+
+  def fetch_instruction
+    if @program_counter < 0 or @program_counter >= @instructions.length then
+      print "Program Terminated\n"
+      @running = false
+    else
+      return @instructions[@program_counter]
+    end
+    return nil
+  end
+
+  def step
+    if @running then
+      op = fetch_instruction
+      if op != nil
+        exec(op[:op], op[:a], op[:b])
       end
     end
   end
-  @start_index +=1
+
+  def blocked?
+    return @io_blocking
+  end
+
+  def running?
+    return @running
+  end
+
+  def run
+    while @running do
+      step
+    end
+  end
+
+  def get_send_count
+    return @send_count
+  end
+
+  def compile(filename)
+    $input.chomp.each_line do |line|
+      parts = line.split(" ")
+      @instructions.push({op: parts[0], a: parts[1], b: parts.length > 2 ? parts[2] : nil})
+    end
+  end
 end
 
+vm_a = SoundProcessingUnit.new 0
+vm_b = SoundProcessingUnit.new 1
+vm_a.pair_io(vm_b)
+vm_b.pair_io(vm_a)
+vm_a.compile(ARGV[0])
+vm_b.compile(ARGV[0])
 
-@t1_registers = @registers.dup
-@t2_registers = @registers.dup
-@t2_registers['p'] = 1
-@t1_send = []
-@t2_send = []
-@start_index_1 = 0
-@start_index_2 = 0
-@t2_send_count = 0
-@t1_waiting = false
-@t2_waiting = false
+while vm_a.running? or vm_b.running? do
+  if vm_a.blocked? and vm_b.blocked?
+    print "Deadlock encountered\n"
+    break
+  end
+  vm_a.step
+  vm_b.step
+end
 
-
+print vm_b.get_send_count, "\n"
 # 7112
 
 
